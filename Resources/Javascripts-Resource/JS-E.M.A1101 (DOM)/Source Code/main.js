23fa06c7a779972134ab1a4a0d4cd174330c7980
Quan2409 <dokahcquan2002@gmail.com>
2024-11-04 16:12:02 +0700

    change workspace

---
+++ Resources/Javascripts-Resource/JS-E.M.A1101 (DOM)/Source Code/main.js
@@ -0,0 +1,18 @@
+const images = ['img1.jpg', 'img2.png', 'img3.png'];
+let index = 0;
+ 
+// Add your code here
+// Task 1
+
+backBtn.addEventListener('click', function() {
+  // Add your code here
+  // Task 3.1
+});
+
+nextBtn.addEventListener('click', function() {
+  // Add your code here
+  // Task 3.2
+});
+
+// Add your code here
+// Task 2
